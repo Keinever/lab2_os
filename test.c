f7e923877fc0e97fc7241ed77cc136289f6f3cc7
Aleks <106027171+Keinever@users.noreply.github.com>
2023-12-18 00:41:56 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,22 +1,64 @@
-#include <linux/kernel.h>  
+#include <linux/kernel.h>
+#include <linux/init.h>
 #include <linux/module.h>
+#include <linux/proc_fs.h>
+#include <linux/uaccess.h>
 
-#define MODULE
-#define LINUX
-#define __KERNEL__
+#define PROCFS_MAX_SIZE 1024
+#define PROCFS_NAME "test"
 
-static int __init my_module_init(void) {
-    pr_info("Hello from my kernel module!\n");
+static struct proc_dir_entry *our_proc_file;
+static char procfs_buffer[PROCFS_MAX_SIZE];
+static unsigned long procfs_buffer_size = 0;
+
+static DEFINE_MUTEX(procfs_mutex);
+
+static ssize_t procfile_read(struct file *file, char __user *buffer,
+                              size_t buffer_length, loff_t *offset) {
+    ssize_t len;
+    mutex_lock(&procfs_mutex);
+    len = simple_read_from_buffer(buffer, buffer_length, offset, procfs_buffer, procfs_buffer_size);
+    mutex_unlock(&procfs_mutex);
+    return len;
+}
+
+static ssize_t procfile_write(struct file *file, const char __user *buff,
+                               size_t len, loff_t *off) {
+    mutex_lock(&procfs_mutex);
+    if (len > PROCFS_MAX_SIZE)
+        len = PROCFS_MAX_SIZE;
+    if (copy_from_user(procfs_buffer, buff, len)) {
+        mutex_unlock(&procfs_mutex);
+        return -EFAULT;
+    }
+    procfs_buffer_size = len;
+    mutex_unlock(&procfs_mutex);
+    return len;
+}
+
+static const struct proc_ops proc_file_fops = {
+    .proc_read = procfile_read,
+    .proc_write = procfile_write,
+};
+
+static int __init procfs_init(void) {
+    our_proc_file = proc_create(PROCFS_NAME, 0644, NULL, &proc_file_fops);
+    if (our_proc_file == NULL) {
+        pr_alert("Error: Could not initialize /proc/%s\n", PROCFS_NAME);
+        return -ENOMEM;
+    }
+    pr_info("/proc/%s created\n", PROCFS_NAME);
     return 0;
 }
 
-static void __exit my_module_exit(void) {
-    pr_info("Exiting my kernel module\n");
+static void __exit procfs_exit(void) {
+    proc_remove(our_proc_file);
+    pr_info("/proc/%s removed\n", PROCFS_NAME);
 }
 
-module_init(my_module_init);
-module_exit(my_module_exit);
+module_init(procfs_init);
+module_exit(procfs_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Your Name");
-MODULE_DESCRIPTION("A simple kernel module");
+MODULE_DESCRIPTION("Simple /proc test module");
