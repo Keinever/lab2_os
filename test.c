39fcbe10e59cee410b78cce3e178d323b5417eb5
Aleks <106027171+Keinever@users.noreply.github.com>
2023-12-24 17:15:49 +0300

    Update test.c

--- test.c
+++ test.c
@@ -1,64 +1,106 @@
 #include <linux/kernel.h>
-#include <linux/init.h>
 #include <linux/module.h>
 #include <linux/proc_fs.h>
+#include <linux/sched.h>
 #include <linux/uaccess.h>
-
-#define PROCFS_MAX_SIZE 1024
-#define PROCFS_NAME "test"
-
+#include <linux/version.h>
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0)
+#include <linux/minmax.h>
+#endif
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0)
+#define HAVE_PROC_OPS
+#endif
+#define PROCFS_MAX_SIZE 2048UL
+#define PROCFS_ENTRY_FILENAME "buffer2k"
 static struct proc_dir_entry *our_proc_file;
 static char procfs_buffer[PROCFS_MAX_SIZE];
 static unsigned long procfs_buffer_size = 0;
-
-static DEFINE_MUTEX(procfs_mutex);
-
-static ssize_t procfile_read(struct file *file, char __user *buffer,
-                              size_t buffer_length, loff_t *offset) {
-    ssize_t len;
-    mutex_lock(&procfs_mutex);
-    len = simple_read_from_buffer(buffer, buffer_length, offset, procfs_buffer, procfs_buffer_size);
-    mutex_unlock(&procfs_mutex);
-    return len;
-}
-
-static ssize_t procfile_write(struct file *file, const char __user *buff,
-                               size_t len, loff_t *off) {
-    mutex_lock(&procfs_mutex);
-    if (len > PROCFS_MAX_SIZE)
-        len = PROCFS_MAX_SIZE;
-    if (copy_from_user(procfs_buffer, buff, len)) {
-        mutex_unlock(&procfs_mutex);
-        return -EFAULT;
-    }
-    procfs_buffer_size = len;
-    mutex_unlock(&procfs_mutex);
-    return len;
-}
-
-static const struct proc_ops proc_file_fops = {
-    .proc_read = procfile_read,
-    .proc_write = procfile_write,
-};
-
-static int __init procfs_init(void) {
-    our_proc_file = proc_create(PROCFS_NAME, 0644, NULL, &proc_file_fops);
-    if (our_proc_file == NULL) {
-        pr_alert("Error: Could not initialize /proc/%s\n", PROCFS_NAME);
-        return -ENOMEM;
-    }
-    pr_info("/proc/%s created\n", PROCFS_NAME);
+static ssize_t procfs_read(struct file *filp, char __user *buffer,
+size_t length, loff_t *offset)
+{
+  if (*offset || procfs_buffer_size == 0) {
+    pr_debug("procfs_read: END\n");
+    *offset = 0;
     return 0;
+  }
+  procfs_buffer_size = min(procfs_buffer_size, length);
+  
+  if (copy_to_user(buffer, procfs_buffer, procfs_buffer_size))
+    return -EFAULT;
+  
+  *offset += procfs_buffer_size;
+  pr_debug("procfs_read: read %lu bytes\n", procfs_buffer_size);
+  return procfs_buffer_size;
 }
 
-static void __exit procfs_exit(void) {
-    proc_remove(our_proc_file);
-    pr_info("/proc/%s removed\n", PROCFS_NAME);
+static ssize_t procfs_write(struct file *file, const char __user *buffer,
+                            size_t len, loff_t *off)
+{
+  procfs_buffer_size = min(PROCFS_MAX_SIZE, len);
+  if (copy_from_user(procfs_buffer, buffer, procfs_buffer_size))
+    return -EFAULT;
+  *off += procfs_buffer_size;
+
+  pr_debug("procfs_write: write %lu bytes\n", procfs_buffer_size);
+  return procfs_buffer_size;
 }
 
-module_init(procfs_init);
-module_exit(procfs_exit);
+static int procfs_open(struct inode *inode, struct file *file)
+{
+  try_module_get(THIS_MODULE);
+  return 0;
+}
+
+static int procfs_close(struct inode *inode, struct file *file)
+{
+  module_put(THIS_MODULE);
+  return 0;
+}
+
+#ifdef HAVE_PROC_OPS
+static struct proc_ops file_ops_4_our_proc_file = {
+  .proc_read = procfs_read,
+  .proc_write = procfs_write,
+  .proc_open = procfs_open,
+  .proc_release = procfs_close,
+};
+#else
+static const struct file_operations file_ops_4_our_proc_file = {
+  .read = procfs_read,
+  .write = procfs_write,
+  .open = procfs_open,
+  .release = procfs_close,
+};
+#endif
+
+static int __init procfs3_init(void)
+{
+  our_proc_file = proc_create(PROCFS_ENTRY_FILENAME, 0644, NULL,
+                              &file_ops_4_our_proc_file);
+
+  if (our_proc_file == NULL) {
+    remove_proc_entry(PROCFS_ENTRY_FILENAME, NULL);
+
+    pr_debug("Error: Could not initialize /proc/%s\n",
+             PROCFS_ENTRY_FILENAME);
+  
+    return -ENOMEM;
+  }
+
+  proc_set_size(our_proc_file, 80);
+  proc_set_user(our_proc_file, GLOBAL_ROOT_UID, GLOBAL_ROOT_GID);
+
+  pr_debug("/proc/%s created\n", PROCFS_ENTRY_FILENAME);
+  return 0;
+}
+
+static void __exit procfs3_exit(void)
+{
+  remove_proc_entry(PROCFS_ENTRY_FILENAME, NULL);
+  pr_debug("/proc/%s removed\n", PROCFS_ENTRY_FILENAME);
+}
+
+module_init(procfs3_init);
+module_exit(procfs3_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Your Name");
-MODULE_DESCRIPTION("Simple /proc test module");
